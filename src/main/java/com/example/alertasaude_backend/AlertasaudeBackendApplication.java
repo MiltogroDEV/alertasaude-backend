e8436f53ace5d4bbb0b847974aebb58261d35f0d
Emilton Neto <emilton.code@gmail.com>
2025-09-20 11:49:12 -0300

    Spring Initializer

---
+++ src/main/java/com/example/alertasaude_backend/AlertasaudeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.alertasaude_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertasaudeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertasaudeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/alertasaude_backend/AlertasaudeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alertasaude_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertasaudeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
